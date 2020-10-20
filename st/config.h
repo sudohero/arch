755dbac1a996ba6f2192663585291d6fe049bbf8
Heath Stewart <heath.stewart1@gmail.com>
2020-10-20 15:43:23 -0400

    minor updates

--- st/config.h
+++ st/config.h
@@ -114,24 +114,24 @@ float alpha = 0.94;
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
 	"#282828", /* hard contrast: #1d2021 / soft contrast: #32302f */
-	"#cc241d",
-	"#98971a",
-	"#d79921",
-	"#458588",
-	"#b16286",
-	"#689d6a",
-	"#a89984",
+	"#af0000",
+	"#878700",
+	"#af8700",
+	"#005f87",
+	"#875f87",
+	"#5f8787",
+	"#af5f00",
 	"#928374",
-	"#fb4934",
-	"#b8bb26",
-	"#fabd2f",
-	"#83a598",
-	"#d3869b",
-	"#8ec07c",
-	"#ebdbb2",
+	"#d75f5f",
+	"#afaf00",
+	"#ffaf00",
+	"#87afaf",
+	"#d787af",
+	"#87af87",
+	"#ff8700",
 	[255] = 0,
 	/* more colors can be added after 255 to use with DefaultXX */
-	"#add8e6", /* 256 -> cursor */
+	"#87afaf", /* 256 -> cursor */
 	"#555555", /* 257 -> rev cursor*/
 	"#282828", /* 258 -> bg */
 	"#ebdbb2", /* 259 -> fg */
